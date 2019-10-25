95182134052dc1f7690c484c82c5697844caaf2b
Xinju Dan <rvxky@sina.com>
2019-10-25 14:39:33 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2035,7 +2035,8 @@ var cnames_active = {
   "zonayed": "zonayedpca.github.io/js.zonayed.me",
   "zplugins": "zlanguage.github.io/plugin-docs",
   "zty": "zetaoyang.github.io",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "dxj": "danxinju.github.io"
   /*
   * please don't add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
